caab8d52a23eee43e8b0f7dad780549fd4ee4211
Roerick Sweeney <sweeney@roerick.me>
2020-10-18 10:54:00 -0500

    change nutcracker title

--- blog/src/pages/nutcracker.js
+++ blog/src/pages/nutcracker.js
@@ -12,7 +12,7 @@ const Covid = props => (
       <section id="one">
         <div className="inner">
           <header className="major">
-            <h1>Covid 19 Update</h1>
+            <h1>Nutcracker Suite 2020</h1>
           </header>
           <p>
             St. Croix Ballet is pleased to announce that we will be performing a
